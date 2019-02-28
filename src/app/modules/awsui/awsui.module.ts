8a0bc2c79560848cbc6ea3e5d9412a3d350de815
Roger Borges <vacatola@gmail.com>
2019-02-28 06:51:25 -0800

    follow up to last commit

--- src/app/modules/awsui/awsui.module.ts
+++ src/app/modules/awsui/awsui.module.ts
@@ -4,6 +4,7 @@ import * as material from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+//import {SharedUiModule} from '../sharedui/sharedui.module';
 import { AwsCredentialsComponent  } from './components/aws.credentials/aws.credentials.component';
 
 import {AwsModule} from '../aws/aws.module';
@@ -15,6 +16,7 @@ import {AwsModule} from '../aws/aws.module';
     CommonModule,
     FormsModule,
     BrowserAnimationsModule,
+    //SharedUiModule,
     AwsModule,
     //material
     material.MatButtonModule, material.MatCheckboxModule, material.MatListModule, material.MatCardModule, material.MatFormFieldModule,material.MatInputModule,material.MatExpansionModule, 
--- src/app/modules/awsui/components/aws.credentials/aws.credentials.component.ts
+++ src/app/modules/awsui/components/aws.credentials/aws.credentials.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject, Output, EventEmitter } from '@angular/core';
+import { MatDialogModule, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import {IAM} from 'aws-sdk';
 
@@ -26,10 +27,61 @@ export class AwsCredentialsComponent implements OnInit {
     @Inject('aws_config') private config: IAM.ClientConfiguration,
     private regionService: AwsRegionService, 
     private credentialService: AwsCredentialsService
+    , private md: MatDialog
+  ) { 
 
-  ) { }
-
-  ngOnInit() {
   }
+
+  async ngOnInit() {
+    let credentials = await this.credentialService.getCredentials();
+    let defaultRegion: string = null;
+    
+    if (credentials) {
+        this.accesskey = credentials.accessKey;
+        this.secretkey = credentials.secretKey;
+        defaultRegion = credentials.defaultRegion;
+    }
+    await this.regionService.GetRegionsWithConfig({
+        accessKeyId: this.accesskey,
+        secretAccessKey: this.secretkey,
+        region: this.selectedRegion ? this.selectedRegion.value : 'us-west-2'
+    },
+        (myregions) => {
+            this.regions = myregions;
+            if (defaultRegion) {
+                this.regions.forEach(r => {
+                    if (r.value == defaultRegion) {
+                        this.selectedRegion = r;
+                    }
+                });
+            }
+            this.loadingRegions = false;
+        });
+
+    this.saveIntoConfigs();
+
+
+}
+
+public save() {
+  this.credentialService.saveCredentials(
+      this.accesskey,
+      this.secretkey,
+      this.selectedRegion.value,//TODO: Add Region  
+      (err) => { console.log(err); },
+      () => {
+          this.onchange.emit(null);
+          this.md.open(ReloadDialogComponent);
+          this.saveIntoConfigs();
+      }
+  );
+}
+
+private saveIntoConfigs() {
+
+  this.config.accessKeyId = this.accesskey;
+  this.config.secretAccessKey = this.secretkey;
+  this.config.region = this.selectedRegion ? this.selectedRegion.value : null;
+}
 
 }
